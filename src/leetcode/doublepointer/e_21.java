179a6be2891ec4e6c2e441f08cf0b35b503f56b0
zlx <2428784410@qq.com>
2022-07-04 18:41:13 +0800

    双指针解法21题

---
+++ src/leetcode/doublepointer/e_21.java
@@ -0,0 +1,75 @@
+package leetcode.doublepointer;
+
+/**
+ * 21.合并两个有序链表
+ * <p>
+ * 将两个升序链表合并为一个新的 升序 链表并返回。
+ * 新链表是通过拼接给定的两个链表的所有节点组成的。
+ */
+
+class e_21 {
+
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        //虚拟头节点
+        ListNode dummy = new ListNode(-1), p = dummy;
+        ListNode p1 = list1, p2 = list2;
+
+        while (p1 != null && p2 != null) {
+            //比较 p1 和 p2 俩个指针
+            //将值较小的节点接到 p 指针
+            if (p1.val > p2.val) {
+                p.next = p2;
+                p2 = p2.next;
+            } else {
+                p.next = p1;
+                p1 = p1.next;
+            }
+            // p 指针不断前进
+            p = p.next;
+        }
+        if (p1 != null) {
+            p.next = p1;
+        }
+        if (p2 != null) {
+            p.next = p2;
+        }
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+//        ListNode l1_2 = new ListNode(4);
+//        ListNode l1_1 = new ListNode(2);
+        ListNode list1 = new ListNode();
+
+//        ListNode l2_2 = new ListNode(4);
+//        ListNode l2_1 = new ListNode(3,l2_2);
+        ListNode list2 = new ListNode();
+
+        System.out.println(mergeTwoLists(list1, list2));
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return "ListNode{" +
+                "val=" + val +
+                ", next=" + next +
+                '}';
+    }
+}
---
+++ src/leetcode/doublepointer/m_86.java
@@ -0,0 +1,47 @@
+package leetcode.doublepointer;
+
+
+/**
+ * 86. 分隔链表
+ * <p>
+ * 给你一个链表的头节点 head 和一个特定值 x ，请你对链表进行分隔，使得所有 小于 x 的节点都出现在 大于或等于 x 的节点之前。
+ * 你应当 保留 两个分区中每个节点的初始相对位置。
+ */
+class m_86 {
+
+    public static ListNode partition(ListNode head, int x) {
+        ListNode lessNode = new ListNode(-1);
+        ListNode moreNode = new ListNode(-1);
+        //p1 p2 指针负责生成结果链表
+        ListNode p1 = lessNode, p2 = moreNode;
+        //p 负责遍历原链表
+        ListNode p = head;
+        while (p != null) {
+            if (p.val >= x) {
+                p2.next = p;
+                p2 = p2.next;
+            } else {
+                p1.next = p;
+                p1 = p1.next;
+            }
+            //断开原链表中的每个节点的 next 指针 !!!!!!!!!!!!!!!
+            ListNode temp = p.next;
+            p.next = null;
+            p = temp;
+        }
+
+//        lessNode.next = moreNode.next;
+        p1.next = moreNode.next;
+        return lessNode.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode l1_3 = new ListNode(3);
+        ListNode l1_2 = new ListNode(4, l1_3);
+        ListNode l1_1 = new ListNode(2, l1_2);
+        ListNode head = new ListNode(1, l1_1);  //head 1 2 3 4
+        int x = 3;
+        System.out.println(partition(head, x));
+    }
+}
+
